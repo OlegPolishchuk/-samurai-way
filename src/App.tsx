92cdf9d67e63cae4e62fd57f8e9c158e7920a95e
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-03 23:01:17 +0300

    social network 0-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Technologies from "./components/Technologies";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+};
+
+// type MessageType = {
+//     message: string
+// }
+//
+// function HelloMessage(props: MessageType){
+//     return <h1>{props.message}</h1>
+// }
+//
+// const ByeMessage: React.FC<MessageType> = (props)=>{
+//     return <h1>{props.message}</h1>
+// };
+
+
+
 
 export default App;
