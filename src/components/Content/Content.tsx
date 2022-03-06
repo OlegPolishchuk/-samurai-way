5c2492155f8d63d712a6007016e46d60703aae91
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-06 15:23:30 +0300

    fix: edit structure of Profile component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import style from './Content.module.css';
 import Profile from "./Profile/Profile";
-import Posts from "./Posts/Posts";
 
 
 const Content = () => {
     return (
         <div className={`${style.main_style} ${style.main}`}>
             <Profile/>
-            <Posts/>
         </div>
     );
 };
--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import style from './Profile.module.css';
 import Aside from "../../Aside/Aside";
+import Posts from "../Posts/Posts";
 
 const Profile = () => {
     return (
-        <div className={style.profile_wrapper}>
-            <Aside/>
-            <section className={style.profile}>
-                {/*Profile description*/}
-                <div className={style.Profile_name}>
-                    <h2>Profile Name</h2>
-                    <p className={style.profile_status}>set status</p>
-                </div>
-                <div className={style.profile_data}>
-                    <p>City <span>Minsk</span></p>
-                    <p>Age <span>30</span></p>
-                    <p>Marital status <span>single</span></p>
-                </div>
-                <button className={style.edit_btn}>Edit profile</button>
-            </section>
-        </div>
+        <>
+            <div className={style.profile_wrapper}>
+                <Aside/>
+                <section className={style.profile}>
+                    {/*Profile description*/}
+                    <div className={style.Profile_name}>
+                        <h2>Profile Name</h2>
+                        <p className={style.profile_status}>set status</p>
+                    </div>
+                    <div className={style.profile_data}>
+                        <p>City <span>Minsk</span></p>
+                        <p>Age <span>30</span></p>
+                        <p>Marital status <span>single</span></p>
+                    </div>
+                    <button className={style.edit_btn}>Edit profile</button>
+                </section>
+            </div>
+            <Posts/>
+        </>
     );
 };
 
