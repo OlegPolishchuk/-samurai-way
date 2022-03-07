7d3b78915c73d066952fc61936a5578f18f32bec
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-07 10:53:34 +0300

    fix: refactor components structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
-            <Main/>
+            <div className={'wrapper'}>
+                <Navbar/>
+                <Main/>
+            </div>
         </div>
     );
 };
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import style from './Main.module.css';
-import Navbar from "../Navbar/Navbar";
-import Content from "../Content/Content";
+import Profile from "./Profile/Profile";
+import Dialogs from "../Dialogs/Dialogs";
 
 const Main = () => {
     return (
-        <main className={style.main_wrapper +' '+ style.wrapper}>
-            <Navbar/>
-            <Content/>
+        <main className={style.main_wrapper +' main_style'}>
+            {/*<Content/>*/}
+            <Profile/>
+            {/*<Dialogs/>*/}
         </main>
     );
 };
