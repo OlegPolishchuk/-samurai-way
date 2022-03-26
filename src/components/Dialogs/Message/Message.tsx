fcb1775faa9e7328f1952dce118180a5d4f3685b
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-26 16:01:54 +0300

    fix: 30. export MessageType from App

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -1,14 +1,16 @@
 import style from "../Dialogs.module.css";
 import React from "react";
+import {MessageType} from "../../../App";
 
-type MessagePropsType = {
-    id: number
-    name: string
-    avatar: string
-    message: string
-}
+// type MessagePropsType = {
+//     id: number
+//     name: string
+//     avatar: string
+//     message: string
+// }
+// Экспортируем MessageType из App
 
-export const Message = (props: MessagePropsType) => {
+export const Message = (props: MessageType) => {
 
     return (
         <div className={style.message_item}>
