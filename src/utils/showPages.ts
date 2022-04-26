7f326198030e5d6a7e22dac011962d7dc1a3a267
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-26 22:34:19 +0300

    fix: 55. pagination logic.

--- src/utils/showPages.ts
+++ src/utils/showPages.ts
@@ -1,10 +1,13 @@
 export const showPages = (pagesCount: number, currentPage: number, pages: Array<number>, interval: number = 5) => {
     /*interval - количество показанных страниц: interval = 5 => 1... 2 3 4 5 6 ...100  (Не включая первую и последнюю
     * , они будут показаны всегда. По умолчанию interval = 5. С каждыцй стороны от currentPage будет по 2 элемента
-    * + первый и последний*/
-
+    * + первый и последний.
+    * Индекс массива начинается с 0, а страницы с 1. Высчитаем количество элементов слева и справа,
+    * затем interval уменьшим на еденицу, что бы соответствовало индексам массива!!!*/
     let partOfPages = Math.floor(interval / 2)
     let result: Array<number> = [];
+
+    interval -= 1
 
     if (pagesCount <= interval) {
         result =  [...pages]
@@ -12,7 +15,8 @@ export const showPages = (pagesCount: number, currentPage: number, pages: Array<
     else {
         if (currentPage < interval ) {
             if (currentPage === 1) {
-                result = pages.slice(0,interval + partOfPages-1).concat(pagesCount)
+
+                result = pages.slice(0,interval + partOfPages).concat(pagesCount)
             }
             else if (currentPage - partOfPages === 0) {
                 result =  pages.slice(0, interval + partOfPages).concat(pagesCount)
@@ -23,10 +27,10 @@ export const showPages = (pagesCount: number, currentPage: number, pages: Array<
         }
         else {
             if (currentPage + partOfPages === pagesCount) {
-                result = [pages[0]].concat(pages.slice(currentPage-partOfPages))
+                result = [pages[0]].concat(pages.slice(currentPage-interval))
             }
             else if ( currentPage === pagesCount || currentPage >= pagesCount - partOfPages) {
-                result = [pages[0]].concat(pages.slice(pagesCount-interval - 1))
+                result = [pages[0]].concat(pages.slice(pagesCount-interval - partOfPages))
             }
             else {
                 result =  [pages[0]].concat(pages.slice(currentPage - partOfPages-1, currentPage + partOfPages)).concat(pagesCount)
