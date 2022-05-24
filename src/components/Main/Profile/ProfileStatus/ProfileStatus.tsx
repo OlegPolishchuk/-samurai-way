f22ba0ef97747425f1745f03011efc01dca7421d
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-05-24 19:52:49 +0300

    feat: 71. add style to ProfileStatus component. Add onDoubleClickHandler()

--- src/components/Main/Profile/ProfileStatus/ProfileStatus.tsx
+++ src/components/Main/Profile/ProfileStatus/ProfileStatus.tsx
@@ -1,17 +1,35 @@
 import React, {useState} from 'react';
-import style from "../ProfileInfo/ProfileInfo.module.css";
 import {useAppSelector} from "../../../../hooks/hooks";
+import s from './ProfileStatus.module.css'
 
 const ProfileStatus = () => {
     const [editMode, setEditMode] = useState(false)
 
     const status = useAppSelector(state => state.profilePage.profile.aboutMe)
 
+    const onDblClickHandler = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+        setEditMode(true)
+    }
+
+    const onBlurHandler = (e: React.FocusEvent<HTMLParagraphElement>) => {
+        setEditMode(false)
+    }
+
     return (
         <>
             {editMode
-                ? <input type={'text'} value={status}/>
-                : <p className={style.profile_status}>{status}</p>
+                ? <input
+                    className={s.editable_status}
+                    type={'text'}
+                    value={status}
+                    onBlur={onBlurHandler}
+                    onChange={() => {}}
+                    autoFocus
+                />
+                : <p
+                    className={s.profile_status}
+                    onDoubleClick={onDblClickHandler}
+                >{status}</p>
             }
 
         </>
