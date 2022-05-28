231e734acbc29f4145ded90a4872601ab8d02f84
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-05-28 14:19:32 +0300

    feat: 7. add LoginForm component. Add FORMIK

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,24 @@
+import {Form, Formik, Field, FormikHelpers} from 'formik';
 import React from 'react';
 import LoginForm from "./loginForm/LoginForm";
 
+
+export type FormValuesType = {
+    login: string,
+    password: string,
+    rememberMe: boolean
+}
+
 const Login = () => {
+    const onSubmit = (values: FormValuesType,) => {
+        console.log(`values => ${JSON.stringify(values)} `)
+        console.dir(values)
+    }
+
     return (
         <div>
             <h1>Login</h1>
-            <LoginForm />
+            <LoginForm onSubmit={onSubmit}/>
         </div>
     );
 };
--- src/components/login/loginForm/LoginForm.tsx
+++ src/components/login/loginForm/LoginForm.tsx
@@ -1,29 +1,55 @@
 import React from 'react';
+import {Formik, Field, Form} from "formik";
+import {FormValuesType} from "../Login";
 import s from './LoginForm.module.css'
 
-const LoginForm = () => {
+type PropsType = {
+    onSubmit: (values: FormValuesType) => void
+}
+
+function validateField(value: string) {
+    let error = ''
+
+    if (!value) {
+        error = 'Required'
+    }
+
+    return error
+}
+
+const LoginForm: React.ComponentType<PropsType> = (props) => {
+
     return (
-        <form className={s.loginForm}>
-            <label>
-                <input
-                type={'text'}
-                placeholder={'Login'}
-                />
-            </label>
-            <label>
-                <input
-                type={'text'}
-                placeholder={'Password'}
-                />
-            </label>
-            <label>
-                <input
-                type={'checkbox'}
-                />
-                Remember me
-            </label>
-            <button>Login</button>
-        </form>
+        <Formik
+            initialValues={{
+                login: '',
+                password: '',
+                rememberMe: false
+            }}
+            // onSubmit={(values: FormValuesType, {setSubmitting}: FormikHelpers<FormValuesType>) => onSubmit(values, setSubmitting)}
+            onSubmit={values => props.onSubmit(values)}
+        >
+            {({errors, touched, isValidating}) => (
+                <Form className={s.loginForm}>
+                    <label>
+                        <Field name={'login'} placeholder={'login'} validate={validateField}/>
+                        {errors.login && touched.login && <div className={s.error}>{errors.login}</div>}
+                    </label>
+                    <label>
+                        <Field name={'password'} placeholder={'password'} validate={validateField}/>
+                        {errors.password && touched.password && <div className={s.error}>{errors.password}</div>}
+                    </label>
+                    <label>
+                        <Field
+                            type={'checkbox'}
+                            name={'rememberMe'}
+                        />
+                        Remember me
+                    </label>
+                    <button type={'submit'}>Login</button>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
